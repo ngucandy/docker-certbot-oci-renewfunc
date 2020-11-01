1e0f88e570b50ef7a40483ae7eec192c5c086bce
ncandy <andy@moeandandy.com>
2020-11-01 09:41:24 -0500

    Allow DNS subdomain

--- auth.sh
+++ auth.sh
@@ -3,18 +3,21 @@
 # http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -euo pipefail
 
-echo "** PRE AUTHORIZATION HOOK **"
+echo "** PRE AUTHORIZATION HOOK START **"
 quote=$'\042'
 
 # Create DNS TXT record
+echo "** CREATING DNS TXT RECORD **"
 oci --auth resource_principal dns record domain update \
     --force \
     --zone-name-or-id $CERTBOT_FN_DNS_ZONE_ID \
-    --domain _acme-challenge.$CERTBOT_FN_DOMAIN \
-    --items "[{${quote}domain${quote}: ${quote}_acme-challenge.${CERTBOT_FN_DOMAIN}${quote}, \
+    --domain _acme-challenge.$CERTBOT_FN_DNS_ZONE_NAME \
+    --items "[{${quote}domain${quote}: ${quote}_acme-challenge.${CERTBOT_FN_DNS_ZONE_NAME}${quote}, \
                ${quote}rdata${quote}: ${quote}${CERTBOT_VALIDATION}${quote}, \
                ${quote}rtype${quote}: ${quote}TXT${quote}, \
                ${quote}ttl${quote}: 60}]"
 
 # Give the record some time to propogate
 sleep 20
+
+echo "** PRE AUTHORIZATION HOOK FINISH **"
--- cleanup.sh
+++ cleanup.sh
@@ -3,10 +3,13 @@
 # http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -euo pipefail
 
-echo "** CLEANUP HOOK **"
+echo "** CLEANUP HOOK START **"
 
 # Remove DNS TXT record
+echo "** REMOVING DNS TXT RECORD **"
 oci --auth resource_principal dns record domain delete \
     --force \
     --zone-name-or-id $CERTBOT_FN_DNS_ZONE_ID \
-    --domain _acme-challenge.$CERTBOT_FN_DOMAIN
+    --domain _acme-challenge.$CERTBOT_FN_DNS_ZONE_NAME
+
+echo "** CLEANUP HOOK FINISH **"
