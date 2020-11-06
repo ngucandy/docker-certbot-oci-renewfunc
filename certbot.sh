d2b365c202b9e74620e84025a3557ee15b581cb2
ncandy <andy@moeandandy.com>
2020-11-06 10:21:48 -0500

    Cleanup logging messages

--- certbot.sh
+++ certbot.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo "** CERTBOT **"
-echo
 
 certbot_base_dir="/tmp/certbot"
 config_dir="$certbot_base_dir/config"
@@ -11,13 +10,17 @@ logs_dir="$certbot_base_dir/logs"
 mkdir -p ${logs_dir}
 echo "** STDIN DUMP **"
 /usr/bin/tee ${logs_dir}/event.json
-echo
 
 certbot_archive="${CERTBOT_FN_ARCHIVE_FILE_PREFIX}-${CERTBOT_FN_DOMAIN}.tar.gz"
-if oci --auth resource_principal os object head -ns $CERTBOT_FN_OS_NS -bn $CERTBOT_FN_OS_BN --name "${certbot_archive}"; then
+echo "** CHECKING FOR EXISTING CERTBOT ARCHIVE **"
+if oci --auth resource_principal os object head -ns $CERTBOT_FN_OS_NS -bn $CERTBOT_FN_OS_BN --name "${certbot_archive}" > /dev/null; then
+  echo "** DOWNLOADING EXISTING CERTBOT ARCHIVE **"
   oci --auth resource_principal os object get -ns $CERTBOT_FN_OS_NS -bn $CERTBOT_FN_OS_BN --name "${certbot_archive}" --file "-" | tar -C /tmp -xz
+  echo "** RENEWING CERTIFICATES IN CERTBOT ARCHIVE **"
   /usr/local/bin/certbot renew --config-dir $config_dir --work-dir $work_dir --logs-dir $logs_dir
 else
+  echo "** NO EXISTING CERTBOT ARCHIVE **"
+  echo "** REQUESTING NEW CERTIFICATE **"
   /usr/local/bin/certbot certonly --manual --preferred-challenges=dns --agree-tos \
     --manual-auth-hook /auth.sh \
     --manual-cleanup-hook /cleanup.sh \
@@ -32,17 +35,10 @@ else
     --test-cert
 fi
 
-#if [ -s /tmp/input.txt ]; then
-#  echo "** DOWNLOAD EXISTING ARCHIVE FROM OBJECT STORAGE **"
-#  certbot_archive_old=$(python3 -c "import sys, json, re; print(re.sub('\.control$', '', json.load(sys.stdin)['data']['resourceName']))")
-#  oci --auth resource_principal os object get -ns $CERTBOT_FN_OS_NS -bn $CERTBOT_FN_OS_BN --name "${certbot_archive_old}" --file "/tmp/${certbot_archive_old}"
-#  tar -C /tmp -xzf "/tmp/${certbot_archive_old}"
-#fi
-
-echo "** UPLOAD ARCHIVE TO OBJECT STORAGE **"
-echo
-certbot_control="${certbot_archive}.control"
+echo "** UPLOADING CERTBOT ARCHIVE **"
 tar -C /tmp -czf "/tmp/$certbot_archive" certbot
 oci --auth resource_principal os object put -ns $CERTBOT_FN_OS_NS -bn $CERTBOT_FN_OS_BN --file "/tmp/$certbot_archive" --force
+echo "** UPLOADING CERTBOT CONTROL FILE **"
+certbot_control="${certbot_archive}.control"
 touch "/tmp/$certbot_control"
 oci --auth resource_principal os object put -ns $CERTBOT_FN_OS_NS -bn $CERTBOT_FN_OS_BN --file "/tmp/$certbot_control" --force
